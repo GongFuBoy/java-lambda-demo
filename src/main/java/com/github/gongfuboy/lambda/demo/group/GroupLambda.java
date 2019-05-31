8ca565b6ccf2e229e3eb4a8479badeb06eccdcc4
liming_zhou <liming_zhou@sui.com>
2019-05-31 20:10:09 +0800

    add  grouping lambda use case

---
+++ src/main/java/com/github/gongfuboy/lambda/demo/group/GroupLambda.java
@@ -0,0 +1,36 @@
+package com.github.gongfuboy.lambda.demo.group;
+
+import com.github.gongfuboy.lambda.demo.pojo.Person;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * grouping lambda use case
+ *
+ * Created by ZhouLiMing on 2019/5/31.
+ */
+public class GroupLambda {
+
+
+    /**
+     * groupingBy demo
+     */
+    public static void groupingBy() {
+
+        List<Person> personList = Arrays.asList(new Person(18, "Jack"), new Person(19, "Tom"),
+                new Person(18, "Jack"));
+        System.out.println(personList.stream().collect(Collectors.toList()));
+        Map<Pair, List<Person>> groupedMap =
+                personList.stream().collect(Collectors.groupingBy(person -> Pair.of(person.getAge(), person.getName())));
+        System.out.println(groupedMap);
+    }
+
+    public static void main(String[] args) {
+        groupingBy();
+    }
+
+}
---
+++ src/main/java/com/github/gongfuboy/lambda/demo/pojo/Person.java
@@ -0,0 +1,43 @@
+package com.github.gongfuboy.lambda.demo.pojo;
+
+/**
+ * Created by ZhouLiMing on 2019/5/31.
+ */
+public class Person {
+
+    private Integer age;
+
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(Integer age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
